60133a16e48b3c230262508251b7450fe56c7902
Jessica Pope <jessica.pope.co@gmail.com>
2019-09-06 14:56:06 -0600

    refactoring

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,18 +1,42 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant_hash|
+    if contestant_hash["status"].downcase == "winner"
+      return contestant_hash["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+      contestants.each do |info|
+        if info["occupation"] == occupation
+          return info["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |info|
+      if info["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      contestant.find{ |contestant| contestant["hometown"] == hometown }
+    end
+  end
 end
+
 
 def get_average_age_for_season(data, season)
   # code here
